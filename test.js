e72b7bf450a644fdf91343c076b71bc55e98b350
Dmitry_Kuzbass <edv123@rambler.ru>
2026-09-08 10:38:39 +0700

    Итоговые задания

---
+++ test.js
@@ -0,0 +1,10 @@
+const obj = {
+    first: 'First',
+    second: 'Second'
+};
+
+const [firstKey, firstValue] = Object.entries(obj);
+
+console.log('--- РЕЗУЛЬТАТ ЗАДАЧИ ---');
+console.log('Ключ:', firstKey);
+console.log('Значение:', firstValue);
